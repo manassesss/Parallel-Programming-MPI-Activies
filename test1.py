3de8a4da5c41dca9f57a2f4aeae5a2ea38c5f65e
manassesss <manasses3mm@gmail.com>
2021-05-18 09:13:15 -0300

    rank test

---
+++ test1.py
@@ -0,0 +1,15 @@
+from mpi4py import MPI
+import numpy
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 0 :
+    print('my rank is', rank)
+    data = {'a':0 , 'b':1}
+    comm.send(data, dest=1)
+
+if rank == 1:
+    print('my rank is', rank)
+    data = comm.recv(source=0)
+    print(data)
